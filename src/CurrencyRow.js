4fd463c88e1a70046801fbb3cc250129520f77e6
Tadea <tsimunoviczg@gmail.com>
2020-07-03 09:32:07 -0400

    add class name

--- src/CurrencyRow.js
+++ src/CurrencyRow.js
@@ -3,9 +3,9 @@ import React from "react";
 export default function CurrencyRow() {
   return (
     <div>
-      <input type="number" />
+      <input type="number" className="input" />
       <select>
-          <option value="USD">USD</option>
+        <option value="USD">USD</option>
       </select>
     </div>
   );
