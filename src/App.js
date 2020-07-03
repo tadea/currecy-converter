07638367e3ba1363e73cb5839ab502a615f92e9e
Tadea <tsimunoviczg@gmail.com>
2020-07-03 09:16:04 -0400

    created new file for currency row

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import CurrencyRow from "./CurrencyRow";
+
+function App() {
+  return (
+    <div>
+      <h1>Currency Converter</h1>
+      <CurrencyRow />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/CurrencyRow.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function CurrencyRow() {
+  return (
+    <div>
+      <h2>Currency Row</h2>
+    </div>
+  );
+}
