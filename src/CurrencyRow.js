5e325fad884568388492e66462637c0d0b3b2d60
Tadea <tsimunoviczg@gmail.com>
2020-07-03 09:24:06 -0400

    add input field

--- src/CurrencyRow.js
+++ src/CurrencyRow.js
@@ -3,7 +3,10 @@ import React from "react";
 export default function CurrencyRow() {
   return (
     <div>
-      <h2>Currency Row</h2>
+      <input type="number" />
+      <select>
+          <option value="USD">USD</option>
+      </select>
     </div>
   );
 }
