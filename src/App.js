f354ccac71da4fcf8a5b234d43fd69846ddfa4c2
Tadea <tsimunoviczg@gmail.com>
2020-07-07 11:45:57 -0400

    useEffect added

--- src/App.js
+++ src/App.js
@@ -34,6 +34,14 @@ function App() {
       });
   }, []);
 
+  useEffect(() => {
+    if (fromCurrency != null && toCurrency != null) {
+      fetch(`${api_call}?base=${fromCurrency}&symbols=${toCurrency}`)
+        .then((response) => response.json())
+        .then((data) => setExchangeRate(data.rates[toCurrency]));
+    }
+  }, [fromCurrency, toCurrency]);
+
   function handleFromAmountChange(e) {
     setAmount(e.target.value);
     setAmountInFromCurrency(true);
