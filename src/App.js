fdf6161a3f68c6876f06cf74cd6be3ffecb978a3
Tadea <tsimunoviczg@gmail.com>
2020-07-07 10:45:08 -0400

    add currency option in fetch call

--- src/App.js
+++ src/App.js
@@ -6,21 +6,24 @@ import { FaBalanceScale } from "react-icons/fa";
 const api_call = "https://api.exchangeratesapi.io/latest";
 
 function App() {
-  const [currencyOptions, setCurrencyOptions] = useState([])
-
+  const [currencyOptions, setCurrencyOptions] = useState([]);
+  console.log(currencyOptions);
 
   useEffect(() => {
     fetch(api_call)
       .then((response) => response.json())
-      .then((data) => console.log(data));
+      .then((data) =>
+        setCurrencyOptions([data.base, ...Object.keys(data.rates)])
+      );
   }, []);
+
   return (
     <div className="container">
       <div className="header">
         <h1>Currency Converter</h1>
       </div>
       <CurrencyRow />
-      <div className="equals">
+      <div className="scale">
         <FaBalanceScale />
       </div>
       <CurrencyRow />
